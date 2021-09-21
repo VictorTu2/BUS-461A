3dde9f911e99a1a7e25df64d7f0239351baa5d82
VictorTu2 <victorvvtu@gmail.com>
2021-09-21 10:56:55 -0700

    Deleted some lines of code

--- Project 1.R
+++ Project 1.R
@@ -6,29 +6,3 @@ df <- read.csv(file = '/Users/victortu/Desktop/ BUS 461/BUS-461A/Csv files/bank-
 head(df)
 
 colnames(df)
-
-# Q1. Develop a flow chart to strategize the customer contacts you could make to
-# bring in more customers subscribing to a term deposit.
-# The flow chart should include a threshold value of number of customers who 
-# could take certain threshold of term deposits based on your intuition.
-# Create a alternate strategy of flow, if the threshold is not met. 
-# Submit this flow chart as a separate word document with the honors pledge on 
-# first page (30 points)
-
-install.packages("DiagrammeR")
-library(DiagrammeR)
-
-grViz(diagram = "digraph flowchart 
-{
-  node [fontname = arial, shape = oval]
-  tab1 [label = '@@1']
-  tab2 [label = '@@2']
-  tab3 [label = '@@3']
-  
-  tab1 -> tab2 -> tab3;
-}
-  
-  [1]: 'Learning Data Science'
-  [2]: 'Industry vs Technical Knowledge'    
-  [3]: 'Statistics vs Mathematics Knowledge'    
-  ")
